fb6009c3ed94f362db8793cde023e2356df60d43
SvenD <sdelariv@gmail.com>
2021-12-10 09:51:48 +0100

    Started on creating methods

--- src/main/java/be/svend/goodviews/services/UserService.java
+++ src/main/java/be/svend/goodviews/services/UserService.java
@@ -4,6 +4,8 @@ import be.svend.goodviews.models.User;
 import be.svend.goodviews.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
     UserRepository userRepo;
@@ -12,5 +14,13 @@ public class UserService {
         this.userRepo = userRepo;
     }
 
+    // CREATE METHODS
 
+    public Optional<User> createUser(User user) {
+
+        if (!validAsNew(user)) return Optional.empty();
+        userRepo.save(user);
+
+        return Optional.of(user);
+    }
 }
