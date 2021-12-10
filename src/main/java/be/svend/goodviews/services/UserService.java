98c167804ae342deabb06ccee9b6c1965ae3ef48
SvenD <sdelariv@gmail.com>
2021-12-10 09:49:43 +0100

    Set up UserService basics

--- src/main/java/be/svend/goodviews/services/UserService.java
+++ src/main/java/be/svend/goodviews/services/UserService.java
@@ -1,7 +1,16 @@
 package be.svend.goodviews.services;
 
+import be.svend.goodviews.models.User;
+import be.svend.goodviews.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    UserRepository userRepo;
+
+    public UserService(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+
 }
