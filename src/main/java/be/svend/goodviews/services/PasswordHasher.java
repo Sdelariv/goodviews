357b4385df3ca65c195e47ae3e8f69fef204ce52
SvenD <sdelariv@gmail.com>
2021-12-14 10:03:06 +0100

    Added PasswordHasher (empty)

---
+++ src/main/java/be/svend/goodviews/services/PasswordHasher.java
@@ -0,0 +1,12 @@
+package be.svend.goodviews.services;
+
+public class PasswordHasher {
+
+
+    public static String hashPassword(String password) {
+
+        String passwordHash = ""; // TODO: fill in
+
+        return passwordHash;
+    }
+}
