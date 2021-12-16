849bfd3293e640067e7a3994df7b9fe3cebca5b3
SvenD <sdelariv@gmail.com>
2021-12-16 11:49:29 +0100

    First trial of friend creation methods

--- src/main/java/be/svend/goodviews/services/users/FriendshipService.java
+++ src/main/java/be/svend/goodviews/services/users/FriendshipService.java
@@ -5,6 +5,7 @@ import be.svend.goodviews.models.User;
 import be.svend.goodviews.repositories.FriendshipRepository;
 import be.svend.goodviews.repositories.UserRepository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 public class FriendshipService {
@@ -21,12 +22,24 @@ public class FriendshipService {
 
     // CREATE METHODS
 
-    public void requestFriendship (User userA, String username) {
-        // TODO: fill in
+    public boolean requestFriendship (User userA, String username) {
+        Optional<User> requestedFriend = userRepo.findByUsername(username);
+        if (requestedFriend.isEmpty()) return false;
+
+        createRequestedFriendship(userA,requestedFriend.get());
+        return true;
     }
 
+    public Optional<Friendship> acceptFriendship(Friendship friendship) {
+        Optional<Friendship> friendshipInDb = friendshipRepo.findById(friendship.getId());
+        if (friendshipInDb.isEmpty()) return Optional.empty();
 
-    public Optional<Friendship> createFriendship(User userA, User userB) {
+        friendshipInDb.get().setAccepted(true);
+        friendshipInDb.get().setDateCreated(LocalDate.now());
+        return saveFriendship(friendshipInDb.get());
+    }
+
+    public Optional<Friendship> createRequestedFriendship(User userA, User userB) {
         // Check if users exist
         Optional<User> friendA = userRepo.findByUsername(userA.getUsername());
         if (friendA.isEmpty()) return Optional.empty();
@@ -42,7 +55,17 @@ public class FriendshipService {
         friendship.setFriendA(friendA.get());
         friendship.setFriendB(friendB.get());
 
+        // Add notification
+        // TODO: fill in
+
         return saveFriendship(friendship);
+    }
+
+    public Optional<Friendship> createFriendship(User userA, User userB) {
+        Optional<Friendship> requestedFriendship = createRequestedFriendship(userA,userB);
+        if (requestedFriendship.isEmpty()) return Optional.empty();
+
+        return acceptFriendship(requestedFriendship.get());
     }
 
     // INTERNAL METHODS
