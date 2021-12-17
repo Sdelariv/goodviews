32ad619496453c517f591fa6af770fba769df6c9
SvenD <sdelariv@gmail.com>
2021-12-17 15:27:36 +0100

    Added TagSuggestionRepo

---
+++ src/main/java/be/svend/goodviews/repositories/notification/TagSuggestionRepository.java
@@ -0,0 +1,12 @@
+package be.svend.goodviews.repositories.notification;
+
+import be.svend.goodviews.models.Film;
+import be.svend.goodviews.models.notification.TagSuggestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TagSuggestionRepository extends JpaRepository<TagSuggestion, Long> {
+
+    Optional<TagSuggestion> findByFilmAndSuggestedTag(Film film, String suggestedTag);
+}
