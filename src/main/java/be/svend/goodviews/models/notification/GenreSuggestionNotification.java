d5124f12f0d953dc50a77b48516d504ac7926182
SvenD <sdelariv@gmail.com>
2021-12-17 10:04:31 +0100

    Added GenreSuggestionNotification class

---
+++ src/main/java/be/svend/goodviews/models/notification/GenreSuggestionNotification.java
@@ -0,0 +1,81 @@
+package be.svend.goodviews.models.notification;
+
+import be.svend.goodviews.models.Film;
+import be.svend.goodviews.models.TypeOfUser;
+import be.svend.goodviews.models.User;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+@Entity
+public class GenreSuggestionNotification extends Notification {
+
+    private String suggestedGenreName;
+
+    @OneToOne
+    private User suggester;
+
+    @OneToOne
+    private Film film;
+
+    // CONSTRUCTOR
+
+    public GenreSuggestionNotification(){
+        super.setTypeOfUser(TypeOfUser.ADMIN);
+    }
+
+    public GenreSuggestionNotification(String suggestedGenreName, Film film, User suggester) {
+        this.film = film;
+        this.suggester = suggester;
+        this.suggestedGenreName = suggestedGenreName;
+        super.setTypeOfUser(TypeOfUser.ADMIN);
+    }
+
+    // GETTERS & SETTERS
+
+    public String getSuggestedGenreName() {
+        return suggestedGenreName;
+    }
+
+    public void setSuggestedGenreName(String suggestedGenreName) {
+        this.suggestedGenreName = suggestedGenreName;
+        updateMessage();
+    }
+
+    public User getSuggester() {
+        return suggester;
+    }
+
+    public void setSuggester(User suggester) {
+        this.suggester = suggester;
+        updateMessage();
+    }
+
+    public Film getFilm() {
+        return film;
+    }
+
+    public void setFilm(Film film) {
+        this.film = film;
+        updateMessage();
+    }
+
+    public void updateMessage() {
+        if (suggester == null || suggester.getUsername() == null) return;
+        if (film == null || film.getTitle() == null || film.getId() == null) return;
+
+        super.setMessage(suggester.getUsername() + " has suggested the genre \"" + this.suggestedGenreName + "\" for " + film.getTitle() + " (" + film.getId() + ")");
+    }
+
+    // OTHER METHODS
+
+
+    @Override
+    public String toString() {
+        return "GenreSuggestionNotification{" +
+                "suggestedGenreName='" + suggestedGenreName + '\'' +
+                ", suggester=" + suggester +
+                ", film=" + film +
+                "} " + super.toString();
+    }
+}
