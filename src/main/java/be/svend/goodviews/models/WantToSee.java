180de843b2724bae6bf1703f01d617fbf9356a37
SvenD <sdelariv@gmail.com>
2022-01-10 14:54:55 +0100

    Added WantToSee

---
+++ src/main/java/be/svend/goodviews/models/WantToSee.java
@@ -0,0 +1,71 @@
+package be.svend.goodviews.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.time.LocalDateTime;
+
+@Entity
+public class WantToSee {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne
+    private User user;
+
+    @OneToOne
+    private Film film;
+
+    private LocalDateTime dateCreated;
+
+    // GETTERS & SETTERS
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Film getFilm() {
+        return film;
+    }
+
+    public void setFilm(Film film) {
+        this.film = film;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    // OTHER METHODS
+
+
+    @Override
+    public String toString() {
+        return "WantToSee{" +
+                "id=" + id +
+                ", user=" + user +
+                ", film=" + film +
+                ", dateCreated=" + dateCreated +
+                '}';
+    }
+}
